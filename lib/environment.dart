cfaa521b41653b4d2b885d64b80ba0155c09acb7
zke <kzews3824@gmail.com>
2023-12-01 16:58:34 +0300

    change to fake_auth_api

--- lib/environment.dart
+++ lib/environment.dart
@@ -2,5 +2,5 @@
 // //const THYROID_API_URL = 'http://178.124.209.20/thyroid-app';
 // const AUTH_API_URL = 'http://localhost:8080/auth-api';
 const AUTH_API_URL = 'http://178.124.209.20/thyroid-auth';
-const USE_FAKE_THYROID_API = true;
-const USE_FAKE_AUTH_API = false;
+const USE_FAKE_THYROID_API = false;
+const USE_FAKE_AUTH_API = true;
